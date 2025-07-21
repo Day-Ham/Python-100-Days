1394a837ccfa482f33053bf46b2324a265755e32
Day-Ham <daehankimm17@gmail.com>
2025-07-21 13:08:37 +0800

    Day 1: Band Name Generator

---
+++ Name Generator.py
@@ -0,0 +1,6 @@
+print("What was your City that you were born In")
+FirstName =input()
+print("What was your pets name")
+LastName = input()
+
+print(FirstName + " " + LastName +" is your band name")
