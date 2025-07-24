f8decefd7f6a4b02510a5b0a0c1da95ac4d97626
Day-Ham <daehankimm17@gmail.com>
2025-07-24 19:10:40 +0800

    Personal Playground: Slots Machine

--- main.py
+++ main.py
@@ -1,59 +1,15 @@
 import os
+import random
+import time
 
-
-print("Do you want to Encode Or Decode a word")
-valid= False
-Decision=''
-while valid==False:
-   Decision= input()
-   if Decision == 'encode':
-      valid=True
-   elif Decision == 'decode':
-      valid=True
-   else:
-      print("Input Invalid")
-
-
-
-def Encode():
-   print("Write the Word to Encrypt")
-   WordToEncrypt=input()
-   print("Input the Number of Shifts")
-   OffsetNumber=int(input())
-   CypherWord=''
-   for letter in WordToEncrypt:
-      if letter.islower():
-         base = ord('a')
-         shifted  =(ord(letter) - base + OffsetNumber) % 26
-         EncryptedLetter = chr(shifted + base)
-         CypherWord +=EncryptedLetter
-      else:
-         base = ord('A')
-         shifted  =(ord(letter) - base + OffsetNumber) % 26
-         EncryptedLetter = chr(shifted + base)
-         CypherWord +=EncryptedLetter   
-   print(CypherWord)
-def Decode():
-   print("Write the Word to Decode")
-   WordToEncrypt=input()
-   print("Input the Number of Shifts")
-   OffsetNumber=int(input())
-   CypherWord=''
-   for letter in WordToEncrypt:
-      if letter.islower():
-         base = ord('a') #Use as the reference Archor of the 
-         shifted  =(ord(letter) - base - OffsetNumber) % 26
-         EncryptedLetter = chr(shifted + base)
-         CypherWord +=EncryptedLetter
-      else:
-         base = ord('A')
-         shifted  =(ord(letter) - base - OffsetNumber) % 26
-         EncryptedLetter = chr(shifted + base)
-         CypherWord +=EncryptedLetter   
-   print(CypherWord)
-if Decision == 'encode':
-      Encode()
-elif Decision == 'decode':
-      Decode()
-else:
-   print("None")
+Duration=0
+while Duration<30:
+   icons = ["🍒", "🍋", "🔔", "🍇", "🍉", "⭐", "7️⃣", "💎", "🍀", "🎰"]
+   Result=[]
+   for index in range(3):
+      Result.append(icons[random.randint(0,len(icons)-1)])
+   os.system('cls')
+   print(Result)
+   time.sleep(0.1)
+   Duration+=1
+ 
