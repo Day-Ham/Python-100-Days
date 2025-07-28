a925f5297b7023bbc7d937a7e0691a779fff50a2
Day-Ham <daehankimm17@gmail.com>
2025-07-28 13:11:40 +0800

    Black Jack Majority Functions Set Up

--- main.py
+++ main.py
@@ -1,23 +1,55 @@
+import random
+import os
 
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+PlayerTotal=0
+PlayerTotalLower=0
+DealerTotal=0
+DealerTotalLower=0
+PlayerHand=[]
+DealerHand=[]
+command='hit'
+def ConvertToTotal(hand):
+    total=0
+    for card in hand:
+        if card== 'J' or card== 'Q' or card== 'K':
+            total+=10
+        elif card== 'A':
+            total
+        else:
+            total += int(card)
+    
+    return total
+        
+while command =='hit':
+    os.system('cls')
+    PlayerHand.append(ranks[random.randint(0,len(ranks)-1)])
+    PlayerTotal= ConvertToTotal(PlayerHand)
+    print(PlayerHand)
+    print("Total is: " + str(PlayerTotal))
+    if PlayerTotal >21:
+        print("Player Bust")
+        break
+    elif PlayerTotal == 21:
+        print("Black Jack Player Wins")
+        break
+    print("Would you like to Hit or Stand")
+    command=input()
 
-def add(first, second):
-    return first+second
+if PlayerTotal<21:
+    while DealerTotal<21:
+        os.system('cls')
+        DealerHand.append(ranks[random.randint(0,len(ranks)-1)])
+        print("Dealer Hand")
+        print(DealerHand)
+        print("Player Hand")
+        print(PlayerHand)
+        DealerTotal= ConvertToTotal(DealerHand)
+        print("Dealer Total is: " + str(DealerTotal))
+        print("Player Total is: " + str(PlayerTotal))
+        if DealerTotal==21:
+            print("Black Jack Dealer Wins")
+            break
 
-def sub(first, second):
-    return first-second
-
-def mul(first, second):
-    return first*second
-
-def div(first, second):
-    return first/second
-
-print("Input First Number:")
-First= float(input())
-print("Input First Number:")
-Second= float(input())
-
-print("Sum:" + str(add(First,Second)))
-print("Sub:" + str(sub(First,Second)))
-print("Product:" + str(mul(First,Second)))
-print("Divident:" + str(div(First,Second)))
+if DealerTotal>21:
+    print("Dealer Bust, Player Wins")
