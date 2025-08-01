457cdf0e57702814265591716bffcbe212f7a49b
Day-Ham <daehankimm17@gmail.com>
2025-08-01 12:20:06 +0800

    Day 16: Quiz Game

--- Quiz_Trivia/QuizGame.py
+++ Quiz_Trivia/QuizGame.py
@@ -1,8 +1,31 @@
+import os
+import time
 from question_model import Question
 from data import trivia
 Question_Bank=[]
+Lives=3
+
 for item in trivia:
     NewQuestion=Question(item["question"],item["answer"])
     Question_Bank.append(NewQuestion)
+os.system('cls')
+for question in Question_Bank:
+    print("Lives Left: " +str(Lives))
+    print(question.text)
+    answer= str(question.answer).lower()
+    guess =  str(input()).lower()
+    if guess==answer:
+        print("Correct!")
+    else:
+        print("Incorrect!")
+        Lives -=1
+    if Lives==0:
+        break
+    time.sleep(1)
+    os.system('cls')
 
-print(Question_Bank[0].answer)
+os.system('cls')
+if Lives>0:
+    print("You Win!")
+else:
+    print("You Lose!")
